a90744c79f945897747df81780e710b503fba88e
l14d35 <kujawa.dp@gmail.com>
2019-05-05 23:01:51 +0200

    GreetingController fixed

--- SpringBoot01/src/main/java/com/learning/SpringBoot01/controller/GreetingController.java
+++ SpringBoot01/src/main/java/com/learning/SpringBoot01/controller/GreetingController.java
@@ -3,11 +3,14 @@ package com.learning.SpringBoot01.controller;
 import com.learning.SpringBoot01.persistance.Greeting;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+@RestController
 public class GreetingController {
-    private static final String template = "Hello %s!";
+
+    private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
