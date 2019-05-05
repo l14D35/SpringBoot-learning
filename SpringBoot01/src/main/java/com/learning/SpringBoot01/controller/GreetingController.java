26a2abfc6652535c211ce501ef4d1490d38d8b27
l14d35 <kujawa.dp@gmail.com>
2019-05-05 22:35:12 +0200

    GreetingController added

--- SpringBoot01/src/main/java/com/learning/SpringBoot01/controller/GreetingController.java
+++ SpringBoot01/src/main/java/com/learning/SpringBoot01/controller/GreetingController.java
@@ -1,4 +1,17 @@
 package com.learning.SpringBoot01.controller;
 
+import com.learning.SpringBoot01.persistance.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.atomic.AtomicLong;
+
 public class GreetingController {
+    private static final String template = "Hello %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
 }
